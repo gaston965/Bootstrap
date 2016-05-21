71130e0b995839c75ffde4c73e6e65f6b628d7fe
root <root@CIE>
2016-05-21 04:25:55 -0300

    pudevalidaruncampo

--- validacion.js
+++ validacion.js
@@ -1,11 +1,62 @@
 $(document).ready(function(){
-
+    
 $("#miBoton").click(function()
-{
-  alert("lalalala");
-});
-});
+{      $("#NOSE").toggle("hide");
+	if($("#IDPROVEEDOR").val() == ""){
+        $("#NOSE").toggle("show");
+        $("#IDPROVEEDOR").addClass('resaltar');       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+       
+  
+        $("#IDPROVEEDOR").click(function(){
+        $("#NOSE").toggle("hide")}); 
+           
+       return false;
+     }
 
-	
-	
+
+
+
+
+     
+   });});
+
+$(document).ready(function()
+	 
+{  
+    // $("#APHELP").addClass('hide');
+	$("#miBoton").click(function()
+{
+	if($("#APELLIDOPROVEEDOR").val() == ""){
+        // $("#APHELP").addClass('show');
+        $("#APELLIDOPROVEEDOR").addClass('resaltar');       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+       
+  
+        $("#APELLIDOPROVEEDOR").click(function(){
+        // $("#APHELP").addClass('hide')
+              }); 
+           
+       return false;
+     }
+
+
+
+
+
+     
+   });});
+
+
+       
+
+        
+     
+    
+         
+
+       
+        
+     
+    
+         
+
 
