be748d49d2bd7f614bcf447cd18da94875d21987
root <root@CIE>
2016-05-22 01:25:18 -0300

    Validacion completada proveedores

--- validacion.js
+++ validacion.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+    // $('#formulario').addClass('formulario');
     $(".NOSE").addClass('ocultar');
     $(".NOSE2").addClass('ocultar');
     $(".NOSE3").addClass('ocultar');
+     $(".NOSE4").addClass('ocultar');
+     $(".NOSE5").addClass('ocultar');
+     $(".NOSE7").addClass('ocultar');
 $("#miBoton").click(function()
      
 {      
@@ -82,8 +86,77 @@ $("#miBoton").click(function()
 
          $(".NOSE3").removeClass('resaltar');
           $(".NOSE3").addClass('ocultar');
-         return false;    
-   			
+         
+         if($("#DOMICILIOPROVEEDOR").val() == ""){
+        $(".NOSE4").addClass('mostrar');
+        $("#DOMICILIOPROVEEDOR").addClass('resaltar');       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+       
+       
+        $("#DOMICILIOPROVEEDOR").click(function(){
+          $("#DOMICILIOPROVEEDOR").removeClass('resaltar');
+         $(".NOSE4").removeClass('mostrar')}); 
+         
+
+
+     
+   			}
+
+
+         $(".NOSE4").removeClass('resaltar');
+          $(".NOSE4").addClass('ocultar');
+
+
+           if($("#TELEFONOPROVEEDOR").val() == ""){
+        $(".NOSE5").addClass('mostrar');
+        $("#TELEFONOPROVEEDOR").addClass('resaltar');       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+       
+       
+        $("#TELEFONOPROVEEDOR").click(function(){
+          $("#TELEFONOPROVEEDOR").removeClass('resaltar');
+         $(".NOSE5").removeClass('mostrar')}); 
+         
+
+
+     
+   			}
+
+
+         $(".NOSE5").removeClass('resaltar');
+          $(".NOSE5").addClass('ocultar');
+
+
+           if($("#CORREOPROVEEDOR1").val()==""){
+        		$(".NOSE7").addClass('mostrar');
+        		$("#CORREOPROVEEDOR1").addClass('resaltar');       // Esta función coloca el foco de escritura del usuario en el campo Nombre directamente.
+       
+       
+        	$("#CORREOPROVEEDOR1").click(function(){
+          	$("#CORREOPROVEEDOR1").removeClass('resaltar');
+         	$(".NOSE7").removeClass('mostrar')}); 
+         
+
+
+     
+   			}
+
+         
+           
+          
+           
+          
+            $(".NOSE7").removeClass('resaltar');
+            $(".NOSE7").addClass('ocultar');
+
+           
+   			 return false;  
+
+
+
+
+
+
+
+         
 
 
 
@@ -92,6 +165,7 @@ $("#miBoton").click(function()
 
 
          }
+
   
 
       
